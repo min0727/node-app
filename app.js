2f89f94e07cb023b98241f61cb6a540867951920
iMac <mino727@c-able.ne.jp>
2019-08-17 18:21:17 +0900

    fs

--- app.js
+++ app.js
@@ -7,14 +7,11 @@ const fs = require('fs');
 var server = http.createServer(
     (request, response) => {
         fs.readFile('./index.html', 'UTF-8',
-            (error;
-    ))
-        response.setHeader('Content-Type', 'text/html');
-        response.write('<!DOCTYPE html><html lang="ja">');
-        response.write('<head><meta charset="utf-8">');
-        response.write('<title>omo</title></head>');
-        response.write('<body><h1>私はオモです。</h1></body></html>');
-        response.end();
+            (error, data) => {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.write(data);
+                response.end();
+            });
     }
 );
 server.listen(3000);
