35027d7992f3a042cc722b8b6c0531fa80fd0572
iMac <mino727@c-able.ne.jp>
2019-08-19 18:53:41 +0900

    replace

--- text-operation/app.js
+++ text-operation/app.js
@@ -8,10 +8,13 @@ var server = http.createServer(
     (request, response) => {
         fs.readFile('./index.html', 'UTF-8',
             (error, data) => {
+
+                data = data.replace(/dummy_title/g, 'タイトル').replace(/dummy_content/g, '内容');
                 response.writeHead(200, {'Content-Type': 'text/html'});
                 response.write(data);
                 response.end();
             });
     }
 );
-server.listen(3000);
+server.listen(2000);
+//なぜか3000が塞がっていた
