40ad20cb5dbcf29599ba93e2dbd5123e6b481d09
iMac <mino727@c-able.ne.jp>
2019-08-24 18:09:37 +0900

    comment

---
+++ template/app.js
@@ -0,0 +1,21 @@
+/**
+ * node1st
+ * Created by min0727 on 2019-08-12
+ */
+const http = require('http')
+const fs = require('fs')
+const ejs = require('ejs')
+var server = http.createServer(
+  (request, response) => {
+    // メソッド名が変わっている
+    var index_html = fs.readFileSync('./index.ejs', 'UTF-8')
+
+    var data = ejs.render(index_html)
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    response.write(data)
+    response.end()
+  })
+
+server.listen(2000)
