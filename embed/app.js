239f9e78c64e1cf36abe625b3b4a22f94412f41a
iMac <mino727@c-able.ne.jp>
2019-08-26 18:41:58 +0900

    ;

--- embed/app.js
+++ embed/app.js
@@ -2,23 +2,23 @@
  * node1st
  * Created by min0727 on 2019-08-12
  */
-const http = require('http')
-const fs = require('fs')
-const ejs = require('ejs')
+const http = require('http');
+const fs = require('fs');
+const ejs = require('ejs');
 var server = http.createServer(
   (request, response) => {
     // メソッド名が変わっている
-    var index_html = fs.readFileSync('./index.ejs', 'UTF-8')
+    var index_html = fs.readFileSync('./index.ejs', 'UTF-8');
 
     var data = ejs.render(index_html, {
       title: 'Index',
       content: 'サンプル',
-    })
+    });
     response.writeHead(200, {
       'Content-Type': 'text/html'
-    })
-    response.write(data)
-    response.end()
+    });
+    response.write(data);
+    response.end();
   })
 
-server.listen(2000)
+server.listen(2000);
