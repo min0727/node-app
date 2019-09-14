bff86c41e48323481faa388583a6450e8abbdb99
iMac <mino727@c-able.ne.jp>
2019-09-14 11:47:44 +0900

    Bootstrap

--- partItem/app.js
+++ partItem/app.js
@@ -1,34 +1,34 @@
-const http = require('http');
-const fs = require('fs');
-const ejs = require('ejs');
-const url = require('url');
-const qs = require('querystring');
+const HTTP = require('http');
+const FS = require('fs');
+const EJS = require('ejs');
+const URL = require('url');
+const QS = require('querystring');
 // メソッド名が変わっている
-var index_html = fs.readFileSync('./index.ejs', 'UTF-8');
-var other_html = fs.readFileSync('./other.ejs', 'utf8');
-var style_css = fs.readFileSync('./style.css', 'utf8');
-console.log('read');
+const INDEX_HTML = FS.readFileSync('./index.ejs', 'UTF-8');
+const OTHER_HTML = FS.readFileSync('./other.ejs', 'utf8');
+const STYLE_CSS = FS.readFileSync('./css/bootstrap.min.css', 'utf8');
+// console.log('read');
 // /にアクセスすること
-const server = http.createServer((request, response) => {
-  console.log('create');
-  const url_parts = url.parse(request.url, true);
-  console.log('parse');
+const SERVER = HTTP.createServer((request, response) => {
+  // console.log('create');
+  const url_parts = URL.parse(request.url, true);
+  // console.log('parse');
   switch (url_parts.pathname) {
     case '/':
-      console.log('pass');
-      response_index(request, response);
+      // console.log('pass');
+      responseIndex(request, response);
       break;
 
     case '/other':
-      response_other(request, response);
+      responseOther(request, response);
       break;
 
-    case '/style.css':
+    case '/css/bootstrap.min.css':
       // :を忘れるな
       response.writeHead(200, {
         'Content-Type': 'text/css'
       });
-      response.write(style_css);
+      response.write(STYLE_CSS);
       response.end();
       break;
 
@@ -40,24 +40,30 @@ const server = http.createServer((request, response) => {
       break;
   }
 });
-server.listen(2000);
-console.log('start');
+SERVER.listen(2000);
+// console.log('start');
 
-var data = {
+const DATA = {
   '太郎': '09-999-999',
   '花子': '08-888-888',
   '幸子': '07-777-777',
   '一郎': '06-666-666'
 };
+const DATA2 = {
+  '太郎': ['taro@yamada', '09-999-999', 'Tokyo'],
+  '花子': ['hanako@flower', '080-888-888', 'Yokohama'],
+  '幸子': ['sachi@happy', '070-777-777', 'Nagoya'],
+  'イチロー': ['ichiro@sanfrancisco', '060-666-666', 'USA']
+};
 
-function response_index (request, response) {
-  const message = 'これはindexです';
+function responseIndex (request, response) {
+  const MESSAGE = 'これはindexです';
   // console.log('pass');
-  const content = ejs.render(index_html, {
+  const content = EJS.render(INDEX_HTML, {
     title: 'Index',
     // messageに引用符がついてないことに注意
-    content: message,
-    data: data,
+    content: MESSAGE,
+    data: DATA,
     filename: 'data-item'
   });
   response.writeHead(200, {
@@ -68,40 +74,43 @@ function response_index (request, response) {
   // console.log(data);
 }
 
-function response_other (request, response) {
-  var message = 'これはotherです';
-  if (request.method == 'POST') {
-    // 自作のプロパティ
-    var body = '';
-    // データ受信時の処理
-    request.on('data', (data) => {
-      body += data;
-    });
-    request.on('end', () => {
-      // bodyにformの値が全て含まれる
-      var post_data = qs.parse(body);
-      message += 'あなたは「' + post_data.msg + '」と書きました';
+function responseOther (request, response) {
+  const message = 'これはotherです';
+  // if (request.method == 'POST') {
+  //   // 自作のプロパティ
+  //   var body = '';
+  //   // データ受信時の処理
+  //   request.on('data', (data) => {
+  //     body += data;
+  //   });
+  //   request.on('end', () => {
+  //     // bodyにformの値が全て含まれる
+  //     var post_data = QS.parse(body);
+  //     message += 'あなたは「' + post_data.msg + '」と書きました';
 
-      var content = ejs.render(other_html, {
+  const content = EJS.render(OTHER_HTML, {
         title: 'Other',
-        content: message
-      });
-      response.writeHead(200, {
-        'Content-Type': 'text/html'
-      });
-      response.write(content);
-      response.end();
-    });
-  } else {
-    var msg = 'ページが存在しません';
-    var content = ejs.render(other_html, {
-      title: 'Other',
-      content: msg
-    });
-    response.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    response.write(content);
-    response.end();
-  }
+    content: message,
+    data: DATA2,
+    filename: 'data-item.ejs'
+  });
+  response.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  response.write(content);
+  response.end();
 }
+
+// } else {
+//   var msg = 'ページが存在しません';
+//   var content = EJS.render(other_html, {
+//     title: 'Other',
+//     content: msg
+//   });
+//   response.writeHead(200, {
+//     'Content-Type': 'text/html'
+//   });
+//   response.write(content);
+//   response.end();
+// }
+// }
